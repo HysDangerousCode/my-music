92014d7e2fb39b1390d73367f21000b98c73f242
hys81021 <hys81021@163.com>
2022-01-07 00:53:53 +0800

    项目初始化

---
+++ src/test/java/com/hys/mymusic/MyMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hys.mymusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
